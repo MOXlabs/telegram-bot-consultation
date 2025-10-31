5e8c5bb85590c0f2261f5df75b4f274c94431551
MOXlabs <151574932+MOXlabs@users.noreply.github.com>
2025-10-31 11:59:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
     await update.message.reply_text(
-        "Нажмите кнопку ниже чтобы начать заполнение заявки:",
+        "Нажмите кнопку ниже, чтобы начать заполнение заявки:",
         reply_markup=reply_markup
     )
 
